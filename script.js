7180e1936116b290e82ed0f49b9fcde0b5f0b7d7
aldiraspra <aldiras29@gmail.com>
2023-06-29 20:47:19 +0700

    clothing-shop

---
+++ script.js
@@ -0,0 +1,17 @@
+//toggle active
+const navbarNav = document.querySelector
+('.navbar-nav');
+
+//ketika hamburge menu diklik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+//hilangkan sidebar dgn klik diluar sidebar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
